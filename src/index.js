b1ffb45c3e6883401d9b311dd5e7f72c32ed2aaa
Alexis Parave <aparavev@gmail.com>
2023-07-22 18:12:15 -0500

    :sparkles: Initial setup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,31 @@
 const express = require('express')
+const path = require('path')
+const expHbs = require('express-handlebars')
+const methodOverride = require('method-override')
+const session = require('express-session')
 
 const app = express()
 
+//Settings
+app.set('views', path.join(__dirname, 'views'))
+app.engine('.hbs', expHbs.engine({
+  defaultLayout: 'main',
+  layoutDir: path.join(app.get('views'), 'layouts'),
+  partialDir: path.join(app.get('views'), 'partials'),
+  extname: '.hbs'
+}))
+app.set('view engine', '.hbs')
+
+//Middlewares
+app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride('_method'))
+app.use(session({
+  secret: 'mysecretapp',
+  resave: true,
+  saveUninitialized: true
+}))
+
+// Server listening
 app.listen(process.env.PORT || 3000, () => {
   console.log('Server on port', process.env.PORT || 3000)
 })
