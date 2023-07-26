70024681ef373dd6f6673c1609173994136c11f6
Alexis Parave <aparavev@gmail.com>
2023-07-25 22:27:45 -0500

    :lipstick: Add index view for tasks

--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -2,6 +2,11 @@ const express = require('express')
 const router = express.Router()
 const Task = require('../models/Task')
 
+router.get('/', async (req, res) => {
+  const tasks = Task.find()
+  res.render('tasks')
+})
+
 router.get('/new', (req, res) => {
   res.render('tasks/new-task')
 })
@@ -10,7 +15,7 @@ router.post('/', async (req, res) => {
   const { title, description } = req.body
   const newTask = new Task({ title, description })
   await newTask.save()
-  res.send('ok')
+  res.redirect('/tasks')
 })
 
 module.exports = router
