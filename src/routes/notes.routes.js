3b14ef059daca1cd94b44525ddd24923ae180d2f
Alexis Parave <aparavev@gmail.com>
2023-07-25 22:15:47 -0500

    :sparkles: Add functionality to create a new note

--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const router = express.Router()
+const Note = require('../models/Note')
 
 router.get('/new', (req, res) => {
   res.render('notes/new-note')
+})
+
+router.post('/', async (req, res) => {
+  const { title, description } = req.body
+  const newNote = new Note({ title, description })
+  await newNote.save()
+  res.send('ok')
 })
 
 module.exports = router
