5a20974653102197d3b3ad329e67fd69d0f34c80
Alexis Parave <aparavev@gmail.com>
2023-07-27 17:26:00 -0500

    :sparkles: Add validations for users date

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -5,4 +5,25 @@ router.get('/signup', (req, res) => {
   res.render('users/signup')
 })
 
+router.post('/signup', (req, res) => {
+  const { name, email, password, confirm_password } = req.body
+  const errors = []
+  if(!name) errors.push({ message: 'Please write a name' }) 
+  if(!email) errors.push({ message: 'Please write a email' })
+  if(password.length < 4 ) errors.push({ message: 'Password must have at least 6 characters' }) 
+  if(password != confirm_password) errors.push({ message: 'Password does not match' })
+  
+  if (errors.length > 0) {
+    res.render('users/signup', {
+      errors,
+      name,
+      email,
+      password,
+      confirm_password
+    })
+  } else {
+    res.send('ok')
+  }
+})
+
 module.exports = router
