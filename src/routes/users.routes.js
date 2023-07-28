fb3993b8625e56d1c8659152877fdb0b2d10789b
Alexis Parave <aparavev@gmail.com>
2023-07-28 03:42:30 -0500

    :sparkles: Add functionality to sign in

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const User = require('../models/User')
+const passport = require('passport')
 
 router.get('/signup', (req, res) => {
   res.render('users/signup')
@@ -41,5 +42,11 @@ router.post('/signup', async (req, res) => {
 router.get('/signin', (req, res) => {
   res.render('users/signin')
 })
+
+router.post('/signin', passport.authenticate('local', {
+  successRedirect: '/tasks',
+  failureRedirect: '/users/signin',
+  failureFlash: true
+}))
 
 module.exports = router
