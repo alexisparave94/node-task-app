3925c2fc047bc57000d2b32d6317b09db11e2933
Alexis Parave <aparavev@gmail.com>
2023-08-22 14:43:52 -0500

    :recycle: Update validations for sign up

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -7,14 +7,15 @@ const User = require('../models/User')
 router.get('/signup', userController.newSingup)
 
 router.post('/signup',
-  body('name').notEmpty(), 
-  body('email').notEmpty().custom(async value => {
+  body('name').notEmpty().withMessage('Enter a name'), 
+  body('email').notEmpty().withMessage('Enter an email').custom(async value => {
     const user = await User.findOne({ email: value })
     if (user) throw new Error('The email is already registered')
   }),
   body('password').isLength({ min: 6 }).withMessage('Password must have at least 6 characters'),
-  body('confirm_password').custom((value, {req}) => {
+  body('confirm_password').notEmpty().withMessage('Enter a confirmation password').custom((value, {req}) => {
     if (value !== req.body.password) throw new Error('Password does not match')
+    return true
   }),
   userController.singup,
   userController.authenticateSignup
